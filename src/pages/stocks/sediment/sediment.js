25763c3126336b5cc522691d1b88a100972944bb
moeenDehqan <moeen.dehqan@gmail.com>
2022-06-21 23:53:32 +0430

    keepTrader

--- src/pages/stocks/sediment/sediment.js
+++ src/pages/stocks/sediment/sediment.js
@@ -1,6 +1,24 @@
 
+import axios from 'axios'
+import { serverAddress } from '../../../config/config'
+import {getCookie} from '../../../components/cookie'
+import { useEffect, useState} from 'react';
 
 const Sediment = () =>{
+    const username = getCookie('username')
+    const handleSediment = () =>{
+        axios({
+            method: 'POST',
+            url: serverAddress+'/stocks/sediment',
+            data: {
+                username:username,
+                period:1,
+            }
+        }).then(response=>{
+            console.log(response.data)
+        })
+    }
+    useEffect(handleSediment,[])
     return(
         <aside>
             sediment
