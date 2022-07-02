5c213046bb42e62b99ab6cb9bc868e3c1e3eaf8a
moeendehqan <moeen.dehqan@gmail.com>
2022-07-02 15:27:12 +0430

    store

--- src/pages/stocks/update/update.jsx
+++ src/pages/stocks/update/update.jsx
@@ -9,8 +9,7 @@ import MiniLoader from '../../../components/loader/miniloader'
 
 const UpdateStocks = () =>{
     const username = getCookie('username')
-    const [fileTrade, setFileTrade] = useState(null)
-    const [fileRegister, SetFileRegister] = useState(null)
+    const [daily, SetDaily] = useState(null)
     const [msg, setMsg] = useState(null)
     const [bulletin, setBulletin] = useState(null)
     const [unavailable, setUnavailable] = useState(null)
@@ -39,15 +38,12 @@ const UpdateStocks = () =>{
         })
     }
 
-    const handleSubmit = async() =>{
-        if (fileTrade===null){
+    const handleSubmitdaily = async() =>{
+        if (daily===null){
             setMsg('فایل معاملات را بارگذاری کنید')
-        }else if(fileRegister===null){
-            setMsg('فایل ریجیستر را بارگذاری کنید')
         }else{
             const formData = new FormData();
-            formData.append('Trade',fileTrade)
-            formData.append('Register',fileRegister)
+            formData.append('daily',daily)
             formData.append('user',username)
             await axios({
                 method: 'post',
@@ -55,11 +51,11 @@ const UpdateStocks = () =>{
                 data: formData,
                 config: {headers:{'content-type': 'multipart/form-data'}}
             }).then(response=>{
-                setMsg(response.data.msg)
-                if(response.data.replay){
-                    setFileTrade(null)
-                    SetFileRegister(null)
-                }            })        }    }
+                    setMsg(response.data.msg)
+        })        }    }
+
+
+
 
     const handleUnavailebleList= ()=>{
         const text = <p>روز های که بروزرسانی انجام نشده</p>
@@ -81,21 +77,16 @@ const UpdateStocks = () =>{
 
         <div className='dashboard'>
             <div className='UpdateStocks'>
+                  <h3>بروزرسانی</h3>
                 <div>
-                    <h3>بروزرسانی</h3>
                     <div>
                         <label>
-                            <p>فایل معاملات</p>
-                            <input type='file' onChange={e=>setFileTrade(e.target.files[0])}></input>
-                        </label>
-                        <label>
-                            <p>فایل رجیستر</p>
-                            <input type='file' onChange={e=>SetFileRegister(e.target.files[0])}></input>
+                            <p>فایل معاملات روزانه</p>
+                            <input type='file' onChange={e=>SetDaily(e.target.files[0])}></input>
                         </label>
                     </div>
-                        <button onClick={handleSubmit}>ثبت</button>
+                        <button onClick={handleSubmitdaily}>ثبت</button>
                 </div>
-
             </div>
             <Alarm msg={msg} smsg={setMsg}/>
             <div className='updateBulletin'>
